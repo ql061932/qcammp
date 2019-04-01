705c73174b66198714bf561f8269324d4b7b50dd
Y.Kashiwayanagi <youhei_kashiwayanagi@qualica.co.jp>
2019-04-01 19:53:31 +0900

    commit

--- dengon/forms.py
+++ dengon/forms.py
@@ -1,9 +1,23 @@
 from django import forms
 
 class dengonForm(forms.Form):
-    #to
+    #nameTo
     nameTo = forms.CharField(
         label='nameTo',
+        max_length=50,
+        required=True,
+        widget=forms.TextInput()
+    )
+    #nameFrom
+    nameFrom = forms.CharField(
+        label='nameFrom',
+        max_length=50,
+        required=True,
+        widget=forms.TextInput()
+    )
+    #nameTakenBy
+    nameFrom = forms.CharField(
+        label='nameTakenBy',
         max_length=50,
         required=True,
         widget=forms.TextInput()
--- dengon/views.py
+++ dengon/views.py
@@ -6,6 +6,8 @@ from django.http import HttpRequest
 from django.template import RequestContext
 from dengon.models import dengon
 from dengon.forms import dengonForm
+from datetime import datetime as dt
+
 
 def input(request):
 	entities = dengon.objects.all()
@@ -20,17 +22,26 @@ def list(request):
 			return render(request, 'input.html', {'error':1})
 		#create
 		if 'nameTo' in request.POST:
+			print((request.POST['date'] + request.POST['hours'] + request.POST['minutes']))
 			dengon.objects.update_or_create(nameTo=request.POST['nameTo'],
 											nameFrom=request.POST['nameFrom'],
 											nameTakenBy=request.POST['nameTakenBy'],
 											requirement=request.POST['requirement'],
 											phoneNumber=request.POST['phoneNumber'],
+											dateTime=dt.strptime((request.POST['date'] + request.POST['hours'] + request.POST['minutes']), "%Y-%m-%d%H%M"),
 											message=request.POST['message'])
 
 	#from list
 	if 'search' in request.POST:
 		#search with query
-		entities = dengon.objects.filter(nameTo=request.POST['wk.nameTo'])
+		entities = dengon.objects.filter(nameTo__icontains=request.POST['nameTo'],
+			nameTakenBy__icontains=request.POST['nameTakenBy'],
+			dateTime__date=request.POST['dateTime'],
+			check=request.POST['check'])
+		print(entities.query)
+	elif 'update_task' in request.POST:
+		#update_task
+		print('debug')
 	else:
 		#search all
 		entities = dengon.objects.all()
